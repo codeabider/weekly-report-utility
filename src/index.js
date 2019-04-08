9c373844d9d01239ca6f99bf23477f319bedfe34
Harshdeep Bilaiya <harshdeep.bilaiya@globant.com>
2019-04-08 20:31:00 +0530

    added arrow

--- src/index.js
+++ src/index.js
@@ -7,23 +7,40 @@ const colorArray = ['danger', 'neutral', 'success'];
 
 const init = function() {
   drawTable();
-  $('.circle').attr('index', -1);
-  $('.arrow').attr('index', -1);
+  $('.indicator').attr('index', -1);  // circle + arrow
   attachMouseHandlers();
 };
 
 const attachMouseHandlers = function() {
-  $('.circle').on('click', clickHandler);
+  $('.indicator').on('click', clickHandler);
+  $('.show-progress').on('click', showProgress);
+  $('.show-regress').on('click', showRegress);
+  $('.show-all').on('click', showAll);
   $('.download').on('click', downloadReport);
 }
+
+// const downloadReport = function() {
+//   const node = document.getElementById('weekly-report');
+//   domtoimage.toPng(node)
+//   .then( (dataUrl) => {
+//     $('.download').attr('href', dataUrl);
+//   })
+//   .catch( (error) => {
+//       console.error('oops, something went wrong!', error);
+//   });
+// }
 
 const downloadReport = function() {
   const node = document.getElementById('weekly-report');
   domtoimage.toPng(node)
-  .then( (dataUrl) => {
-    $('.download').attr('href', dataUrl);
+  .then(function (dataUrl) {
+      const downloadAnchor = document.createElement('a');
+
+      downloadAnchor.href = dataUrl;
+      downloadAnchor.download = 'report';
+      downloadAnchor.click();
   })
-  .catch( (error) => {
+  .catch(function (error) {
       console.error('oops, something went wrong!', error);
   });
 }
@@ -51,6 +68,18 @@ const drawTable = function() {
     bFilter: false,
     bInfo: false
   });
+}
+
+const showProgress = function() {
+  console.log($('.indicator.success').length);
+}
+
+const showRegress = function() {
+  console.log($('.indicator.danger').length);
+}
+
+const showAll = function() {
+  console.log($('.indicator').length);
 }
 
 init();
