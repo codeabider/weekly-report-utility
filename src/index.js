38240aa8bc9ca96b1caf916735d37bba16382ae3
Harshdeep Bilaiya <harshdeep.bilaiya@globant.com>
2019-04-09 13:06:56 +0530

    code cleanup, optimizations, bug fixes

--- src/index.js
+++ src/index.js
@@ -3,112 +3,89 @@ import './scss/index.scss';
 import 'datatables.net';
 import domtoimage from 'dom-to-image';
 
-const colorArray = ['danger', 'neutral', 'success'];
+(() => {
+  const colorArray = ['safe', 'danger', 'neutral'];
 
-const init = function() {
-  drawTable();
-  $('.indicator').attr('index', -1);  // circle + arrow
-  attachMouseHandlers();
-};
+  const init = () => {
+    drawTable();
+    $('.indicator').attr('index', -1); // circle + arrow
+    attachMouseHandlers();
+  };
 
-const attachMouseHandlers = function() {
-  $('.indicator').on('click', clickHandler);
-  $('.show-progress').on('click', showProgress);
-  $('.show-regress').on('click', showRegress);
-  $('.show-all').on('click', showAll);
-  $('.download').on('click', downloadReport);
-}
+  const attachMouseHandlers = () => {
+    $('.indicator').on('click', clickHandler);
+    $('.download').on('click', downloadReport);
+  }
 
-// const downloadReport = function() {
-//   const node = document.getElementById('weekly-report');
-//   domtoimage.toPng(node)
-//   .then( (dataUrl) => {
-//     $('.download').attr('href', dataUrl);
-//   })
-//   .catch( (error) => {
-//       console.error('oops, something went wrong!', error);
-//   });
-// }
+  const downloadReport = () => {
+    const progressReport = $('#weekly-report').clone();
+    const regressReport = $('#weekly-report').clone();
 
-const downloadReport = function() {
-  const progressReport = $('.weekly-report-table').clone();
-  const regressReport = $('.weekly-report-table').clone();
+    progressReport.attr('id', 'progress-report');
+    regressReport.attr('id', 'regress-report');
 
-  progressReport.attr('id', 'progress-report');
-  regressReport.attr('id', 'regress-report');
+    progressReport.appendTo('.clone-container');
+    regressReport.appendTo('.clone-container');
 
-  progressReport.appendTo('.content-wrapper');
-  regressReport.appendTo('.content-wrapper');
+    buildReports();
 
-  showProgress();
-  showRegress();
+    const nodes = [
+      document.getElementById('weekly-report'),
+      document.getElementById('progress-report'),
+      document.getElementById('regress-report')
+    ];
 
-  const nodes = [
-    document.getElementById('weekly-report'),
-    document.getElementById('progress-report'),
-    document.getElementById('regress-report')
-  ];
+    nodes.forEach((val, index) => {
+      domtoimage.toPng(nodes[index])
+      .then((dataUrl) => {
+          const downloadAnchor = document.createElement('a');
+          downloadAnchor.href = dataUrl;
+          downloadAnchor.download = nodes[index].id;
+          downloadAnchor.click();
 
-  nodes.forEach(function(val, index) {
-    domtoimage.toPng(nodes[index])
-    .then(function (dataUrl) {
-        const downloadAnchor = document.createElement('a');
-  
-        downloadAnchor.href = dataUrl;
-        downloadAnchor.download = nodes[index].id;
-        downloadAnchor.click();
-
-        if (nodes[index].id === 'progress-report') {
-          $('#progress-report').remove();
-        } else if (nodes[index].id === 'regress-report') {
-          $('#regress-report').remove();
-        }
-    })
-    .catch(function (error) {
-        console.error('oops, something went wrong!', error);
+          if (nodes[index].id === 'progress-report') {
+            progressReport.remove();
+          } else if (nodes[index].id === 'regress-report') {
+            regressReport.remove();
+          }
+      })
+      .catch((error) => {
+          console.error('oops, something went wrong!', error);
+      });
     });
-  });
-}
-
-const clickHandler = function(event) {
-  let index = $(event.target).attr('index');
-  $(event.target).attr('index', ++index);
-
-  if (index === colorArray.length - 1) {
-    $(event.target).attr('index', -1);
   }
 
-  for (let i = 0; i < colorArray.length; i++) {
-    if ($(event.target).hasClass(colorArray[i])) {
-      $(event.target).removeClass(colorArray[i]);
+  const clickHandler = (event) => {
+    let index = $(event.target).attr('index');
+    $(event.target).attr('index', ++index);
+
+    if (index === colorArray.length - 1) {
+      $(event.target).attr('index', -1);
     }
+
+    colorArray.forEach((val, i) => {
+      if ($(event.target).hasClass(colorArray[i])) {
+        $(event.target).removeClass(colorArray[i]);
+      }
+    });
+
+    $(event.target).addClass(colorArray[index]);
   }
-  $(event.target).addClass(colorArray[index]);
-}
 
-const drawTable = function() {
-  $('table').dataTable({
-    bSort: false,
-    bPaginate: false,
-    bFilter: false,
-    bInfo: false
-  });
-}
+  const drawTable = () => {
+    $('table').dataTable({
+      bSort: false,
+      bPaginate: false,
+      bFilter: false,
+      bInfo: false
+    });
+  }
 
-const showProgress = function() {
-  // console.log($('.indicator.success').length);
-  $('#progress-report').find('.indicator.neutral').removeClass('neutral');
-  $('#progress-report').find('.indicator.danger').removeClass('danger');
-}
+  const buildReports = () => {
+    $('#progress-report, #regress-report').find('.indicator.neutral').removeClass('neutral');
+    $('#progress-report').find('.indicator.danger').removeClass('danger');
+    $('#regress-report').find('.indicator.safe').removeClass('safe');
+  }
 
-const showRegress = function() {
-  // console.log($('.indicator.danger').length);
-  $('#regress-report').find('.indicator.neutral').removeClass('neutral');
-  $('#regress-report').find('.indicator.success').removeClass('success');
-}
-
-const showAll = function() {
-  console.log($('.indicator').length);
-}
-
-init();
+  init();
+})();
